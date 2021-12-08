4d999e2cc8b79264abd9622f4bb78b9bffd45271
Kuba Flis <jakub.flis@performance-media>
2021-12-08 14:31:31 +0100

    Create Global Style in project

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+const breakpoints = {
+  maxWidth: 1182,
+  fhd: 1920,
+  largeDesktop: 1140,
+  desktop: 992,
+  tablet: 768,
+  mobile: 320,
+};
+
+const theme = {
+  breakpoints,
+};
+
+export default theme;
