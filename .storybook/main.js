46dd3be8e26f75adcf46f508059a58cade4b584b
Kuba Flis <jakub.flis@performance-media>
2021-12-08 14:14:51 +0100

    Setup Project

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../src/**/*.stories.@(tsx|mdx)'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-a11y',
+    '@storybook/addon-essentials',
+  ],
+  staticDirs: ['../public']
+};
---
+++ src/Welcome.stories.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export default { title: 'Welcome' };
+
+export const Welcome: FC = () => (
+  <div>
+    <h1>Yacht Renting Website Storybook</h1>
+  </div>
+);
