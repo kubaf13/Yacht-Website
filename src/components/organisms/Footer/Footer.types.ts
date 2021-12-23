9fe6a3c31744b4d6b6174ba1e6ef37d0f6e1e9c1
Kuba Flis <jakub.flis@performance-media>
2021-12-23 16:46:57 +0100

    Create Footer Component

--- src/components/organisms/Footer/Footer.types.ts
+++ src/components/organisms/Footer/Footer.types.ts
@@ -1,5 +1,7 @@
+import type { FooterColumnTypes } from '@atoms/FooterColumn/FooterColumn.types';
 import type { SocialMediaTypes } from '@atoms/SocialMedia/SocialMedia.types';
 
 export interface FooterTypes {
   socialMedia: SocialMediaTypes;
+  columns?: FooterColumnTypes[];
 }
