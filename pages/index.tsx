ebb9511b08f8828594f6c6d491d194ba4e1842a1
Kuba Flis <jakub.flis@performance-media>
2021-12-27 12:22:09 +0100

    Change index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,5 @@
-import Jumbotron from '@organisms/Jumbotron';
-import { jumbotronMocks } from '@organisms/Jumbotron/Jumbotron.mocks';
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => (
-  <div>
-    <h1>Hello</h1>
-    <Jumbotron {...jumbotronMocks} />
-  </div>
-);
+const Home: NextPage = () => <h1>Hello Yacht</h1>;
 
 export default Home;
