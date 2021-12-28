d349008edc41e605ac0313853e6b09ce891c443a
Kuba Flis <jakub.flis@performance-media>
2021-12-28 12:50:27 +0100

    Change styles

--- src/components/organisms/Cards/Cards.styled.ts
+++ src/components/organisms/Cards/Cards.styled.ts
@@ -4,6 +4,6 @@ export const CardsSection = styled.section`
   align-items: center;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  margin: 0 auto;
   max-width: ${({ theme }) => theme.breakpoints.maxWidth}px;
 `;
