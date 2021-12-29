1244abfc64117ccea84095cf054ca2c4d33621ac
Kuba Flis <jakub.flis@performance-media>
2021-12-29 15:17:21 +0100

    Create ContactForm

---
+++ src/components/molecules/ContactForm/ContactForm.mocks.ts
@@ -0,0 +1,12 @@
+import type { ContactFormTypes } from './ContactForm.types';
+
+export const contactFormMocks: ContactFormTypes = {
+  title: 'Contact Us',
+  nameLabel: 'name',
+  emailLabel: 'email',
+  messageLabel: 'what do you wanna know? ask us',
+  button: {
+    ariaLabel: 'send to our mail',
+    label: 'send to us!',
+  },
+};
---
+++ src/components/molecules/ContactForm/ContactForm.tsx
@@ -0,0 +1,53 @@
+import type { ReactElement } from 'react';
+import React from 'react';
+
+import {
+  FormField,
+  FormWrapper,
+  StyledFormButton,
+  StyledInput,
+  WhitePaper,
+  Wrapper,
+} from './ContactForm.styled';
+import { ContactFormTypes } from './ContactForm.types';
+
+export const ContactForm = ({
+  title,
+  nameLabel,
+  emailLabel,
+  messageLabel,
+  button,
+}: ContactFormTypes): ReactElement => (
+  <Wrapper>
+    <WhitePaper>
+      <h2>{title}</h2>
+      <FormWrapper
+        action="https://formsubmit.co/kubaf13@gmail.com"
+        method="POST"
+      >
+        <FormField>
+          <StyledInput
+            type="text"
+            name="name"
+            required
+            placeholder={nameLabel}
+          />
+        </FormField>
+        <FormField>
+          <StyledInput
+            type="email"
+            name="email"
+            required
+            placeholder={emailLabel}
+          />
+        </FormField>
+        <FormField>
+          <StyledInput type="text" name="message" placeholder={messageLabel} />
+        </FormField>
+        <StyledFormButton type="submit" aria-label={button.ariaLabel}>
+          {button.label}
+        </StyledFormButton>
+      </FormWrapper>
+    </WhitePaper>
+  </Wrapper>
+);
