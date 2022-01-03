4063130f081ac45c32dd50fd09871d41e0283142
Kuba Flis <jakub.flis@performance-media>
2022-01-03 23:05:54 +0100

    Add head

--- pages/yachts/yacht-annabelle.tsx
+++ pages/yachts/yacht-annabelle.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 import { YachtPage } from '@/components/template/YachtPage/YachtPage';
 import { annabelleMocks } from '@/components/template/YachtPage/YachtPage.mocks';
 
-const YachtAnnabelle: NextPage = () => <YachtPage {...annabelleMocks} />;
+const YachtAnnabelle: NextPage = () => (
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>{annabelleMocks.yachtName}</title>
+    </Head>
+    <YachtPage {...annabelleMocks} />
+  </>
+);
 
 export default YachtAnnabelle;
