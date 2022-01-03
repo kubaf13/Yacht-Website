eeb8fd08ad00f737c9c56855983e172cd9819271
Kuba Flis <jakub.flis@performance-media>
2022-01-03 22:22:05 +0100

    Change styles

---
+++ src/components/organisms/RentingDates/RentingDates.styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const BookingWrapper = styled.section`
+  margin-top: 50px;
+  padding: 10px 20px;
+`;
+
+export const Columns = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: flex-start;
+  margin: 10px 0 0 45px;
+  max-width: 550px;
+  h5 {
+    min-width: 110px;
+  }
+`;
--- src/components/organisms/RentingDates/RentingDates.tsx
+++ src/components/organisms/RentingDates/RentingDates.tsx
@@ -2,12 +2,19 @@ import { RentingDate } from '@molecules/RentingDate/RentingDate';
 import type { ReactElement } from 'react';
 import React from 'react';
 
+import { BookingWrapper, Columns } from './RentingDates.styled';
 import { RentingDatesTypes } from './RentingDates.types';
 
 export const RentingDates = ({ bookings }: RentingDatesTypes): ReactElement => (
-  <>
+  <BookingWrapper>
+    <h3>You can rent this yacht on: </h3>
+    <Columns>
+      <h5>Date</h5>
+      <h5>Price</h5>
+    </Columns>
     {bookings.map(rentingDate => (
       <RentingDate key={rentingDate.date} {...rentingDate} />
     ))}
-  </>
+    <div>* if you want another date, please call us</div>
+  </BookingWrapper>
 );
